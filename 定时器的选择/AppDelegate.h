93aeb2b2ac63185a87fcb66b7ac5c6f26fd11fb2
zhangyannan <401119582@qq.com>
2017-01-21 21:51:14 +0800

    Initial Commit

---
+++ 定时器的选择/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  定时器的选择
+//
+//  Created by 张艳楠 on 2017/1/21.
+//  Copyright © 2017年 zhang yannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
