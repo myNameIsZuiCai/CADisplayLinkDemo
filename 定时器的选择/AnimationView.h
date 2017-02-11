87c72b86bef69bb46432ec7186a7bc103047f3ed
zhangyannan <401119582@qq.com>
2017-02-11 23:52:46 +0800

    图片循环下落

---
+++ 定时器的选择/AnimationView.h
@@ -0,0 +1,13 @@
+//
+//  AnimationView.h
+//  定时器的选择
+//
+//  Created by 张艳楠 on 2017/1/21.
+//  Copyright © 2017年 zhang yannan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationView : UIView
+
+@end
---
+++ 定时器的选择/AnimationView.m
@@ -0,0 +1,21 @@
+//
+//  AnimationView.m
+//  定时器的选择
+//
+//  Created by 张艳楠 on 2017/1/21.
+//  Copyright © 2017年 zhang yannan. All rights reserved.
+//
+
+#import "AnimationView.h"
+
+@implementation AnimationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
